c2b837046c4de7d2b1112884820d70f6538f265e
Pascal Cotret <pascal.cotret@gmail.com>
2023-10-02 17:27:16 +0200

    fix missing values for base and size

--- core/pmp/tb/pmp_dmp_tb.sv
+++ core/pmp/tb/pmp_dmp_tb.sv
@@ -74,6 +74,8 @@ module pmp_tb;
         access_type = riscv::ACCESS_READ;
 
         // 1st test batch: PMP allow access
+        base = 16'b00011001_00000000;
+        size = 8;
         conf_addr[0] = P#(.WIDTH(WIDTH), .PMP_LEN(PMP_LEN))::base_to_conf(base, size);
         conf_pmp[0].addr_mode = riscv::NAPOT;
         conf_pmp[0].access_type = riscv::ACCESS_READ | riscv::ACCESS_WRITE | riscv::ACCESS_EXEC;
